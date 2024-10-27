fcc1d02003aaf870a131a37a77f420bf14133d40
--global <prasannap.purohith@gmail.com>
2024-10-26 21:18:44 -0600

    infrastrcture code

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type = string
-  default = "us-east-1"
+  default = "us-west-2"
 }
 
 variable "vpc-name" {
@@ -44,13 +44,13 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  default     = "ami-04dd23e62ed049936" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.large"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
